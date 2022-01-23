594b02e4a0f89fa24e8bdaad510fe2d625421c11
Brenda Cayzac <brendacayzac@gmail.com>
2022-01-23 13:41:26 +0100

    Dynamic page title

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,27 +6,35 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: HomeView,
+    meta: { title: "Home" },
   },
   {
     path: "/commissions",
     name: "Commissions",
     component: () => import("../views/AboutView.vue"),
+    meta: { title: "Commissions" },
   },
   {
     path: "/portfolio",
     name: "Portfolio",
     component: () => import("../views/AboutView.vue"),
+    meta: { title: "Portfolio" },
   },
   {
     path: "/contact",
-    name: "contact",
+    name: "Contact",
     component: () => import("../views/AboutView.vue"),
+    meta: { title: "Contact" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.afterEach((to, from) => {
+  document.title += " - " + to.meta.title;
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: true,
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "Brenda Cayzac",
+    },
+  },
 });
