c0a4f04226df61087f92c66b03d78d7015535787
Brenda Cayzac <brendacayzac@gmail.com>
2023-03-01 13:35:51 +0100

    adapted css for safari

--- src/views/CommissionsView.vue
+++ src/views/CommissionsView.vue
@@ -199,9 +199,12 @@ export default defineComponent({
     figure {
       width: 48%;
       background-color: rgba($bc-cyan, 0.25);
+	    display: flex;
+	    align-items: center;
+	    justify-content: center;
       img {
         width: auto;
-        height: 100%;
+        height: auto;
         object-fit: cover;
       }
     }
